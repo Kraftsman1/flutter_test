593262ceb9fe49d205ece598c07e32da33a4873a
Asigri Shamsu-Deen Al-Heyr <deen@coldsis.net>
2021-10-27 16:43:06 +0000

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _HomeState extends State<Home> {
               child: new Scaffold(
                 backgroundColor: Colors.blueGrey[800],
                 appBar: new AppBar(
-                  backgroundColor: Colors.blueGrey[900],
+                  backgroundColor: Colors.blueGrey[800],
                   automaticallyImplyLeading: false,
                   bottom: new TabBar(
                     tabs: <Widget>[
